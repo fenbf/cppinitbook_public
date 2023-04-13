963620c54b0857c27805e179cecbf433f00341a0
fenbf <bartlomiej.filipek@gmail.com>
2023-04-13 07:54:38 +0200

    header

--- examples/chapter 14 - exercises/ex_3_counted_type_main.cpp
+++ examples/chapter 14 - exercises/ex_3_counted_type_main.cpp
@@ -1,3 +1,9 @@
+// example for "C++ Initialization Story"
+// https://leanpub.com/cppinitbook
+// by Bartlomiej Filipek
+// 2023
+// MIT License
+
 // run at https://wandbox.org/permlink/GuGzTWKF8irN2YLz
 
 #include <iostream>
--- examples/chapter 14 - exercises/ex_4_sales_fix.cpp
+++ examples/chapter 14 - exercises/ex_4_sales_fix.cpp
@@ -1,3 +1,9 @@
+// example for "C++ Initialization Story"
+// https://leanpub.com/cppinitbook
+// by Bartlomiej Filipek
+// 2023
+// MIT License
+
 // run at https://godbolt.org/z/7YYKMj8eW
 
 #include <gtest/gtest.h>
